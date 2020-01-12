9474f6c55d1974f63886726d32c882a545be793e
Thomas Bertz <tabertz@gmail.com>
2020-01-12 21:16:33 +0100

    use real data

--- src/ui/main.js
+++ src/ui/main.js
@@ -1,45 +1,49 @@
+async function getData(sensorIdRequested) {
+    const response = await fetch('http://localhost:3000/data/2020-01-02+0000.csv');
+    const data = await response.text();
+    const dataOut = [];
+
+    const table = data.split('\n').slice(1);
+    table.forEach(row =>  {
+        const columns = row.split(',');
+        const sensorId = columns[0];
+        if (sensorId == sensorIdRequested) {
+            const x = moment.utc(columns[1]);
+            const y = parseFloat(columns[2]);
+            dataOut.push({x, y});
+        }
+    });
+    return dataOut;
+}
+
 async function chartIt() {
+    const dataSensorA = await getData('28-0114543c8aaa');
+    const dataSensorB = await getData('28-011453f72eaa');
+    const dataSensorC = await getData('28-011453d9f5aa');
     const ctx = document.getElementById("tempCanvas").getContext('2d');
     new Chart(ctx, {
         type: 'scatter',
         data: {
             datasets: [{
-                label: 'Sensor A',
-                borderColor: "red",
-                showLine: true,
-                fill: false,
-                data: [{
-                    x: moment.utc('2020-01-12T18:56:00Z'),
-                    y: 0
-                }, {
-                    x: moment.utc('2020-01-12T18:57:00Z'),
-                    y: 2
-                }, {
-                    x: moment.utc('2020-01-12T18:58:00Z'),
-                    y: 10
-                }, {
-                    x: moment.utc('2020-01-12T18:59:00Z'),
-                    y: 5
-                }]
-            },
-            {
-                label: 'Sensor B',
+                label: '8a',
                 borderColor: "blue",
                 showLine: true,
                 fill: false,
-                data: [{
-                    x: moment.utc('2020-01-12T18:56:01Z'),
-                    y: 2
-                }, {
-                    x: moment.utc('2020-01-12T18:57:01Z'),
-                    y: 3
-                }, {
-                    x: moment.utc('2020-01-12T18:58:01Z'),
-                    y: 7
-                }, {
-                    x: moment.utc('2020-01-12T18:59:01Z'),
-                    y: 6
-                }]
+                data: dataSensorA
+            },
+            {
+                label: '2e',
+                borderColor: "green",
+                showLine: true,
+                fill: false,
+                data: dataSensorB
+            },
+            {
+                label: 'f5',
+                borderColor: "red",
+                showLine: true,
+                fill: false,
+                data: dataSensorC
             }]
         },
         options: {
