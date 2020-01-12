167a2cf1780fd643d9fbf9e8ed1d4d46f5f7316d
Thomas Bertz <tabertz@gmail.com>
2020-01-12 20:21:13 +0100

    format time display

--- src/ui/main.js
+++ src/ui/main.js
@@ -1,52 +1,61 @@
-const ctx = document.getElementById("tempCanvas").getContext('2d');
-new Chart(ctx, {
-    type: 'scatter',
-    data: {
-        datasets: [{
-            label: 'Sensor A',
-            borderColor: "red",
-            showLine: true,
-            fill: false,
-            data: [{
-                x: -10,
-                y: 0
-            }, {
-                x: -5,
-                y: 2
-            }, {
-                x: 0,
-                y: 10
-            }, {
-                x: 10,
-                y: 5
+async function chartIt() {
+    const ctx = document.getElementById("tempCanvas").getContext('2d');
+    new Chart(ctx, {
+        type: 'scatter',
+        data: {
+            datasets: [{
+                label: 'Sensor A',
+                borderColor: "red",
+                showLine: true,
+                fill: false,
+                data: [{
+                    x: moment.utc('2020-01-12T18:56:00Z'),
+                    y: 0
+                }, {
+                    x: moment.utc('2020-01-12T18:57:00Z'),
+                    y: 2
+                }, {
+                    x: moment.utc('2020-01-12T18:58:00Z'),
+                    y: 10
+                }, {
+                    x: moment.utc('2020-01-12T18:59:00Z'),
+                    y: 5
+                }]
+            },
+            {
+                label: 'Sensor B',
+                borderColor: "blue",
+                showLine: true,
+                fill: false,
+                data: [{
+                    x: moment.utc('2020-01-12T18:56:01Z'),
+                    y: 2
+                }, {
+                    x: moment.utc('2020-01-12T18:57:01Z'),
+                    y: 3
+                }, {
+                    x: moment.utc('2020-01-12T18:58:01Z'),
+                    y: 7
+                }, {
+                    x: moment.utc('2020-01-12T18:59:01Z'),
+                    y: 6
+                }]
             }]
         },
-        {
-            label: 'Sensor B',
-            borderColor: "blue",
-            showLine: true,
-            fill: false,
-            data: [{
-                x: -9,
-                y: 2
-            }, {
-                x: -4,
-                y: 3
-            }, {
-                x: 1,
-                y: 7
-            }, {
-                x: 11,
-                y: 6
-            }]
-        }]
-    },
-    options: {
-        scales: {
-            xAxes: [{
-                type: 'time',
-                position: 'bottom'
-            }]
+        options: {
+            scales: {
+                xAxes: [{
+                    type: 'time',
+                    time: {
+                        displayFormats: {
+                            second: 'hh:mm:ss'
+                        }
+                    },
+                    position: 'bottom'
+                }]
+            }
         }
-    }
-});
+    });
+}
+
+chartIt();
